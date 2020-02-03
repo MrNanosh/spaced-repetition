3812d3b236df9b01cfc1be14301cd38ab1c19694
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-03 09:46:30 -0800

    initial styling for registration

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import '../../components/RegistrationForm/RegistrationForm';
+import React, {
+  Component
+} from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
-      push: () => {},
-    },
-  }
+      push: () => {}
+    }
+  };
 
   handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
-  }
+    const { history } = this.props;
+    history.push('/login');
+  };
 
   render() {
     return (
-      <section>
-        <p>
-          Practice learning a language with the spaced reptition revision technique.
+      <section className="RegistrationForm__section">
+        <p className="RegistrationForm__tagLine">
+          Practice learning a language
+          with the spaced reptition
+          revision technique.
         </p>
-        <h2>Sign up</h2>
+        <h2 className="RegistrationForm__header">
+          Sign up
+        </h2>
         <RegistrationForm
-          onRegistrationSuccess={this.handleRegistrationSuccess}
+          onRegistrationSuccess={
+            this
+              .handleRegistrationSuccess
+          }
         />
       </section>
     );
   }
 }
 
-export default RegistrationRoute
+export default RegistrationRoute;
