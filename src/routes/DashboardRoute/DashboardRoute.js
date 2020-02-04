5719bf576ad0a1ff2a6da3bea76c0a107a49cfdc
Jon <jonathanjackson424@gmail.com>
2020-02-04 12:42:45 -0500

    added Dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+//import Button from '../../components/Button/Button';
+import { Link } from 'react-router-dom';
 
 class DashboardRoute extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+      
+    }
+  }
+
+
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Spanish</h2>
+
+        <p>Total correct answers: 0</p>
+
+        <h3>Words To Practice</h3>
+        <ul>
+        <li><h4>hola</h4>correct answer count: {/*{word.correct_count}*/}incorrect answer count: {/*{word.incorrect_count}*/}</li>
+        </ul>
+
+        <Link to="/learn">Start Practicing</Link>
+      
       </section>
     );
   }
