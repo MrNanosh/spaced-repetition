c64b44c79794503ebc9195d43683693ca692f693
Jon <jonathanjackson424@gmail.com>
2020-02-07 10:22:08 -0500

    server issues...

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
- API_ENDPOINT : 'https://space-r.herokuapp.com/api' || 'http://localhost:8000/api'
+ API_ENDPOINT : 'http://localhost:8000/api' //'https://space-r.herokuapp.com/api' || 
 }
