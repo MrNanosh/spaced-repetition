84c8b30fa149a5e9c22b39c057ba233b9d6c26f8
Jon <jonathanjackson424@gmail.com>
2020-02-07 11:14:43 -0500

    added api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
- API_ENDPOINT : 'http://localhost:8000/api' //'https://space-r.herokuapp.com/api' || 
+ API_ENDPOINT : 'https://space-r.herokuapp.com/api' || 'http://localhost:8000/api'
 }
